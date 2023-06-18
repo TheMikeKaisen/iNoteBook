7ec320d69d0d098465c45285132cfa85066ff223
TheMikeKaisen <98148929+TheMikeKaisen@users.noreply.github.com>
2023-06-18 17:08:36 +0530

    storing the data into the db using mongoose model

---
+++ backend/readMe.txt
@@ -0,0 +1,6 @@
+Storing data into the DataBase using mongoose model
+
+//const user = User(req.body)
+//user.save()
+here whatever we add in thunder client server body, it will be accessed as user and then
+res.send(req.body) will add the data to the database and we can see it in the mongodb
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,14 @@
 const express = require('express')
-
+const User = require('../models/User')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    const obj = {
-        a:"thios",
-        number : "34"
-
-    }
-    res.json(obj)
+// Create a user using: POST "/api/auth/"
+router.post('/', (req, res) => {
+    console.log(req.body)//print the body at the server
+    //res.send("Hello World")//will send the message to the server
+    const user = User(req.body)
+    user.save()
+    res.send(req.body) // will send the body at the database
 })
 
 module.exports=router;
