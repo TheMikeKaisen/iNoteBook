698a29e3454fe208b4c15f38ad6a0bf9b0b8af01
TheMikeKaisen <98148929+TheMikeKaisen@users.noreply.github.com>
2023-06-25 08:41:16 +0530

    creating login endpoint and sending auth token.

--- backend/readMe.txt
+++ backend/readMe.txt
@@ -1,23 +1,3 @@
-We installed two new packages 
-    1.npm install bcryptjs
-    2.npm install jsonwebtoken
-
-Theory: 
-    bcryptjs: 
-    we don't usually store passwords in the database. Instead what we do is we store a hash that indicates the password.
-    So that even if a hacker get the access to the database, he will not be able to log into the account because he wont have the password. 
-
-    But what some hackers do is that they keep a rainbow table, which have various patterns of the knowing the password.
-    So what the developer does is that they add some characters of their own so it generates a different hash which cannot be tracked by the hacker.
-
-    jsonwebtoken:
-    when the user sends their information to the database, there should be a way to identify if it the user himself. 
-    A JSON Web Token (JWT) is a compact, URL-safe means of representing claims securely between two parties. It is commonly used for authentication and authorization purposes in web applications and APIs. JWTs consist of three parts: a header, a payload, and a signature.
-
-    Header: The header typically consists of two parts: the token type (which is JWT) and the signing algorithm used to generate the signature. It is base64url encoded.
-
-    Payload: The payload contains the claims or statements about an entity (e.g., user) and additional metadata. Claims can include information such as the user's ID, role, permissions, or any other relevant data. The payload is also base64url encoded.
-
-    Signature: The signature is created by combining the encoded header, payload, and a secret key. The secret key is known only to the server that issued the token. The signature is used to verify the integrity of the token and ensure that it has not been tampered with.
-
-    When a client sends a request to a server, it includes the JWT in the request headers or as a parameter. The server can then validate the token by verifying the signature using the secret key. If the signature is valid, the server can trust the information contained in the token and use it to authenticate and authorize the client.
+here, we authentiacate the user access to his account if the credentials match correctly.
+for this purpose we use an asyncronous function 
+    bcrypt.compare(<string>, hash:<string>)
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const { body, validationResult } = require('express-validator'); // validationResult will return the errors in the input data
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs")// including bcrypt package
 const router = express.Router()
 const User = require('../models/User')
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken')// including jsonwebtoken package
 
+const JWT_SECRET = "karthikIsAGoodBOY" // a token will be generated for this secret message
 
 // Create a user using: POST "/api/auth/createuser"
 router.post('/createuser',[
@@ -32,8 +33,6 @@ router.post('/createuser',[
         email: req.body.email, //declaring email of the user
         password:  secPass,
     })
-
-    const JWT_SECRET = "karthikIsAGoodBOY" // a token will be generated for this secret message
     const data = {
         user: {
         id : user.id //as id is the index of the database, it is easy to create token using id
@@ -47,6 +46,39 @@ router.post('/createuser',[
     }
 })
 
+// Authenticating a user using: POST "/api/auth/login"-- to check if the credentials are right or wrong
+router.post('/login',[
+    body('email', 'Please enter valid email address!').isEmail(),
+    body('password', 'Enter the password').exists(),
+],  async (req, res) => {
+    const error = validationResult(req) // will store all the results in the variable 'error'
+    if(!error.isEmpty()){
+        return res.status(500).json({error: error.array()}) // returns the errors as a json file if the error variable is not empty
+    }
+    const {email, password} = req.body;
+    try{
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(500).json({error: "Enter the correct credentials."})
+        }
+
+        const passwordCompare = await bcrypt.compare(password, user.password); // compare the given password with the hash of password from database
+        if(!passwordCompare){
+            return res.status(500).json({error: "Enter the correct credentials."})
+        }
+        const data = {
+            user: {
+            id : user.id //as id is the index of the database, it is easy to create token using id
+            }
+        }
+        const authtoken = jwt.sign(data, JWT_SECRET); // creating token
+        res.json({authtoken}); // returning token 
+        
+    }catch(error){
+        console.log(error.message);
+        res.status(500).send("some error occured.")
+    }
+})
 module.exports=router;
 
 
